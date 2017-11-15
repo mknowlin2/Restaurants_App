33728b0d59f4ce4513269e09b23a543d82c4b394
Michael A. Knowlin II <mknowlin2@gmail.com>
2017-11-15 14:24:38 -0500

    Added serialize functionality to the database.

--- src/database/db_setup.py
+++ src/database/db_setup.py
@@ -13,8 +13,16 @@ class Restaurant(Base):
     __tablename__ = 'restaurant'
 
     '''Columns'''
-    id = Column(Integer, primary_key = True)
-    name = Column(String(80), nullable = False)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False)
+
+    @property
+    def serialize(self):
+        # Returns object data in easily serializeable format
+        return {
+            'id': self.id,
+            'name': self.name
+        }
 
 
 class MenuItem(Base):
@@ -22,15 +30,26 @@ class MenuItem(Base):
     __tablename__ = 'menu_item'
 
     '''Columns'''
-    id = Column(Integer, primary_key = True)
-    name = Column(String(80), nullable = False)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False)
     course = Column(String(250))
     description = Column(String(250))
     price = Column(String(8))
     restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
     restaurant = relationship(Restaurant)
 
+    @property
+    def serialize(self):
+        # Returns object data in easily serializeable format
+        return {
+            'id': self.id,
+            'name': self.name,
+            'course': self.course,
+            'price': self.price,
+            'description': self.description
+        }
 
-#######insert at end of file #######
+
+# insert at end of file #
 engine = create_engine('sqlite:///restaurantmenu.db')
 Base.metadata.create_all(engine)
